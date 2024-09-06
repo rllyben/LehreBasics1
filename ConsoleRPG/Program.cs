0f3562735b9da187727ed92fcfe642f744ff69b5
rllyben <f.benn.mueller@gmail.com>
2024-09-06 11:43:57 +0200

    RPG

---
+++ ConsoleRPG/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hero;
+            int lvl = 1;
+            int hp = 100;
+            int sp = 100;
+
+            Console.WriteLine("please name your caracter to start:");
+            hero = Console.ReadLine();
+            Console.WriteLine("Your character name is: " + hero);
+
+            Console.WriteLine("");
+
+        }
+
+    }
+
+}
