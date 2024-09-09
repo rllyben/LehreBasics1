0b78de8e57634f1960d2f6331362fd2390838e89
rllyben <f.benn.mueller@gmail.com>
2024-09-09 07:31:11 +0200

    Taschenrechner

---
+++ Rechner/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Rechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float zahl1= 0;
+            float zahl2= 0;
+            int error = 0;
+            string rechenzeichen = "";
+
+            Console.WriteLine("Zahl 1:");
+            try
+            {
+                zahl1 = float.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                error = 1;
+            }
+
+            Console.WriteLine("Rechenzeichen (+ - * /)");
+            rechenzeichen = Console.ReadLine();
+
+            Console.WriteLine("Zahl 2:");
+            try
+            {
+                zahl2 = float.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                error = 1;
+            }
+
+            if (rechenzeichen == "+" && error != 1)
+            {
+                Console.WriteLine(zahl1 + zahl2);
+            }
+            else if (rechenzeichen == "-" && error != 1)
+            {
+                Console.WriteLine(zahl1 - zahl2);
+            }
+            else if (rechenzeichen == "*" && error != 1)
+            {
+                Console.WriteLine(zahl1 * zahl2);
+            }
+            else if (rechenzeichen == "/" && error != 1)
+            {
+                Console.WriteLine(zahl1 / zahl2);
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe");
+            }
+
+        }
+
+    }
+
+}
