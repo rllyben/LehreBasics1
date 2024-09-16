b65573a8194f35f3e23cfc20a84a266fd369c31e
rllyben <b.yumitoya@gmail.com>
2024-09-16 15:03:42 +0200

    ggg

--- Textetventure1/Program.cs
+++ Textetventure1/Program.cs
@@ -1,36 +1,17 @@
 ﻿namespace Textetventure1
-{
-    internal class Program
     {
-        static void Main(string[] args)
+        internal class Program
         {
-
-            Console.WriteLine("Du befindest dich in der Küche!");
-            Console.WriteLine("Was willst du tun?");
-            Console.WriteLine("1. Ich gehe in's Wohnzimmer      3. Ich gehe in den Flur");
-            Console.WriteLine("2. Ich gehe in den Garten        4. Ich mach mir ein Sandwitch");
-            Console.WriteLine("Exit with 0");
-
-            int action = 0;
-
-            try
+            static void Main(string[] args)
             {
-                action = int.Parse(Console.ReadLine());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                action = 0;
-            }
 
-
-            if (action == 1)
-            {
-                Console.WriteLine("Du gehst in's Wohnzimmer!");
-                Console.WriteLine("Du bist im Wohnzimmer, was willst du tun?");
-                Console.WriteLine("1. Ich setze mich auf die Coutch         3. Ich streichel meine Katze");
-                Console.WriteLine("2. Ich lege mich auf die Coutch          4. Ich gehe in den Flur");
+                Console.WriteLine("Du befindest dich in der Küche!");
+                Console.WriteLine("Was willst du tun?");
+                Console.WriteLine("1. Ich gehe in's Wohnzimmer      3. Ich gehe in den Flur");
+                Console.WriteLine("2. Ich gehe in den Garten        4. Ich mach mir ein Sandwitch");
                 Console.WriteLine("Exit with 0");
+
+                int action = 0;
 
                 try
                 {
@@ -41,12 +22,14 @@
                     Console.WriteLine(ex.Message);
                     action = 0;
                 }
+
+
                 if (action == 1)
                 {
-                    Console.WriteLine("Du setzt dich auf die Coutch");
-                    Console.WriteLine("Was willst du tun?");
-                    Console.WriteLine("1. Ich lege mich auf die Coutch         2. Ich streichel meine Katze");
-                    Console.WriteLine("3. Ich gehe in den Flur");
+                    Console.WriteLine("Du gehst in's Wohnzimmer!");
+                    Console.WriteLine("Du bist im Wohnzimmer, was willst du tun?");
+                    Console.WriteLine("1. Ich setze mich auf die Coutch         3. Ich streichel meine Katze");
+                    Console.WriteLine("2. Ich lege mich auf die Coutch          4. Ich gehe in den Flur");
                     Console.WriteLine("Exit with 0");
 
                     try
@@ -58,12 +41,12 @@
                         Console.WriteLine(ex.Message);
                         action = 0;
                     }
-
                     if (action == 1)
                     {
-                        Console.WriteLine("Du legst dich auf die Coutch");
+                        Console.WriteLine("Du setzt dich auf die Coutch");
                         Console.WriteLine("Was willst du tun?");
-                        Console.WriteLine("1. Ich streichel meine Katze         2. Ich gehe in den Flur");
+                        Console.WriteLine("1. Ich lege mich auf die Coutch         2. Ich streichel meine Katze");
+                        Console.WriteLine("3. Ich gehe in den Flur");
                         Console.WriteLine("Exit with 0");
 
                         try
@@ -76,6 +59,62 @@
                             action = 0;
                         }
 
+                        if (action == 1)
+                        {
+                            Console.WriteLine("Du legst dich auf die Coutch");
+                            Console.WriteLine("Was willst du tun?");
+                            Console.WriteLine("1. Ich streichel meine Katze         2. Ich gehe in den Flur");
+                            Console.WriteLine("Exit with 0");
+
+                            try
+                            {
+                                action = int.Parse(Console.ReadLine());
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                action = 0;
+                            }
+
+                            if (action == 1)
+                            {
+                                Console.WriteLine("Du streichelst diene Katze");
+                                Console.WriteLine("Du hast das Ziel des Spiels erreicht!");
+                                Console.WriteLine("Exit with 0");
+
+                                try
+                                {
+                                    action = int.Parse(Console.ReadLine());
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                    action = 0;
+                                }
+                                if (action == 0)
+                                {
+                                    Console.WriteLine("now exiting");
+                                }
+                            }
+                        }
+
+                    }
+                    else if (action == 2)
+                    {
+                        Console.WriteLine("Du legst dich auf die Coutch");
+                        Console.WriteLine("Was willst du tun?");
+                        Console.WriteLine("1. Ich streichel meine Katze");
+                        Console.WriteLine("Exit with 0");
+
+                        try
+                        {
+                            action = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            action = 0;
+                        }
                         if (action == 1)
                         {
                             Console.WriteLine("Du streichelst diene Katze");
@@ -97,25 +136,7 @@
                             }
                         }
                     }
-
-                }
-                else if (action == 2)
-                {
-                    Console.WriteLine("Du legst dich auf die Coutch");
-                    Console.WriteLine("Was willst du tun?");
-                    Console.WriteLine("1. Ich streichel meine Katze");
-                    Console.WriteLine("Exit with 0");
-
-                    try
-                    {
-                        action = int.Parse(Console.ReadLine());
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                        action = 0;
-                    }
-                    if (action == 1)
+                    else if (action == 3)
                     {
                         Console.WriteLine("Du streichelst diene Katze");
                         Console.WriteLine("Du hast das Ziel des Spiels erreicht!");
@@ -134,12 +155,37 @@
                         {
                             Console.WriteLine("now exiting");
                         }
+
+                    }
+                    else if (action == 4)
+                    {
+                        Console.WriteLine("Du gehst in den Flur");
+                        Console.WriteLine("Du bist im Flur, was willst du tun?");
+                        Console.WriteLine("1. Ich gehe in's Wohnzimmer          2. Ich gehe in den Garten");
+                        Console.WriteLine("Exit with 0");
+
+                        try
+                        {
+                            action = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            action = 0;
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("now exeting!");
                     }
                 }
-                else if (action == 3)
+                else if (action == 2)
                 {
-                    Console.WriteLine("Du streichelst diene Katze");
-                    Console.WriteLine("Du hast das Ziel des Spiels erreicht!");
+                    Console.WriteLine("Du gehst in den Garten!");
+                    Console.WriteLine("Du bist im Garten, was willst du tun?");
+                    Console.WriteLine("1. Ich lege mich auf den Rasen           3. Ich gehe in's Wohnzimmer");
+                    Console.WriteLine("2. Ich gehe in den Flur                  4. Ich grüße den Nachbarn");
                     Console.WriteLine("Exit with 0");
 
                     try
@@ -151,13 +197,9 @@
                         Console.WriteLine(ex.Message);
                         action = 0;
                     }
-                    if (action == 0)
-                    {
-                        Console.WriteLine("now exiting");
-                    }
 
                 }
-                else if (action == 4)
+                else if (action == 3)
                 {
                     Console.WriteLine("Du gehst in den Flur");
                     Console.WriteLine("Du bist im Flur, was willst du tun?");
@@ -175,74 +217,32 @@
                     }
 
                 }
+                else if (action == 4) 
+                { 
+                    Console.WriteLine("Du machst dir ein Sandwitch");
+                    Console.WriteLine("Was willst du tun?");
+                    Console.WriteLine("1. Ich gehe in's Wohnzimmer      3. Ich gehe in den Flur");
+                    Console.WriteLine("2. Ich gehe in den Garten        4. Ich esse das Sandwitch");
+                    Console.WriteLine("Exit with 0");
+
+                    try
+                    {
+                        action = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        action = 0;
+                    }
+
+                }
                 else
                 {
                     Console.WriteLine("now exeting!");
                 }
-            }
-            else if (action == 2)
-            {
-                Console.WriteLine("Du gehst in den Garten!");
-                Console.WriteLine("Du bist im Garten, was willst du tun?");
-                Console.WriteLine("1. Ich lege mich auf den Rasen           3. Ich gehe in's Wohnzimmer");
-                Console.WriteLine("2. Ich gehe in den Flur                  4. Ich grüße den Nachbarn");
-                Console.WriteLine("Exit with 0");
 
-                try
-                {
-                    action = int.Parse(Console.ReadLine());
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    action = 0;
-                }
-
-            }
-            else if (action == 3)
-            {
-                Console.WriteLine("Du gehst in den Flur");
-                Console.WriteLine("Du bist im Flur, was willst du tun?");
-                Console.WriteLine("1. Ich gehe in's Wohnzimmer          2. Ich gehe in den Garten");
-                Console.WriteLine("Exit with 0");
-
-                try
-                {
-                    action = int.Parse(Console.ReadLine());
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    action = 0;
-                }
-
-            }
-            else if (action == 4) 
-            { 
-                Console.WriteLine("Du machst dir ein Sandwitch");
-                Console.WriteLine("Was willst du tun?");
-                Console.WriteLine("1. Ich gehe in's Wohnzimmer      3. Ich gehe in den Flur");
-                Console.WriteLine("2. Ich gehe in den Garten        4. Ich esse das Sandwitch");
-                Console.WriteLine("Exit with 0");
-
-                try
-                {
-                    action = int.Parse(Console.ReadLine());
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    action = 0;
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("now exeting!");
             }
 
         }
 
     }
-
-}
